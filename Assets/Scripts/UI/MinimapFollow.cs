ae550476d352f0028b611cc4b7b11f1fcefedd64
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-06-03 10:27:30 +0300

    Story Dialogs added, some assets and sprites added, minimap added

---
+++ Assets/Scripts/UI/MinimapFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    private void Start()
+    {
+        if (!player) player = GameObject.FindWithTag("Player").transform;
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x, 100, player.position.z);
+    }
+}
