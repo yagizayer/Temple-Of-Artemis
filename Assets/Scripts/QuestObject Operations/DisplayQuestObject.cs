56585a6cebbade9bb72d403880af8990e29e79e6
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-06-09 01:03:21 +0300

    Created DisplayQuestObject script

---
+++ Assets/Scripts/QuestObject Operations/DisplayQuestObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class DisplayQuestObject : MonoBehaviour
+{
+    public QuestObject_SO MyQuestObject;
+
+    private MeshFilter _meshFilter;
+    private MeshRenderer _meshRenderer;
+    private void Start()
+    {
+        SetAttributes();
+    }
+    
+    [ContextMenu("SetAttributes")]
+    public void SetAttributes()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshFilter.mesh = MyQuestObject.mesh;
+
+        _meshRenderer.materials = MyQuestObject.Materials;
+        transform.localScale = Vector3.one * MyQuestObject.Scale;
+    }
+}
