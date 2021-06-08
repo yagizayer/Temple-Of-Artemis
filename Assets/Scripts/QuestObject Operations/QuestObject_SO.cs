f3aa5b940d82e87cd81b1b760999563dd90e7081
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-06-09 01:03:34 +0300

    Update QuestObject_SO.cs

--- Assets/Scripts/QuestObject Operations/QuestObject_SO.cs
+++ Assets/Scripts/QuestObject Operations/QuestObject_SO.cs
@@ -1,8 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+[CreateAssetMenu(fileName = "New QuestObject", menuName = "QuestObject")]
 public class QuestObject_SO : ScriptableObject
 {
-    
+    [Header("Basic Attributes")]
+    [FormerlySerializedAs("Model")]
+    public Mesh mesh;
+    public Material[] Materials;
+    [Range(.001f, 100f)] public float Scale = 1;
+    [Header("Dialog Attributes")]
+    public string Name;
+    public Npcs QuestNpc;
+    [TextArea] public string[] QuestLines;
+
 }
