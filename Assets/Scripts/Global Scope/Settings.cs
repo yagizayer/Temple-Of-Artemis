cf07794195683944836799c35f5e2670582f2b99
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-06-16 11:49:22 +0300

    Created Settings Script Removed Testing script

---
+++ Assets/Scripts/Global Scope/Settings.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using Cinemachine;
+
+public class Settings : MonoBehaviour
+{
+    #region SettingsVariables
+    [SerializeField] private Slider EffectSounds;
+    [SerializeField] private Slider MusicSounds;
+    [SerializeField] private Toggle InvertCamYCheckSign;
+    private static float _effectSoundLevel = 15;
+    private static float _musicSoundLevel = 15;
+    private static bool _isCamYInverted = false;
+    public static float EffectSoundLevel => _effectSoundLevel;
+    public static float MusicSoundLevel => _musicSoundLevel;
+    public static bool IsCamYInverted => _isCamYInverted;
+
+    #endregion
+
+    #region OutputVariables
+    [SerializeField] private CinemachineFreeLook MainCam;
+    [SerializeField] private SoundManagement soundManagement;
+
+    #endregion
+
+
+    public void SetEffectSounds(Slider value)
+    {
+        EffectSounds = value;
+        _effectSoundLevel = EffectSounds.value;
+
+    }
+    public void SetMusicSounds(Slider value)
+    {
+        MusicSounds = value;
+        _musicSoundLevel = MusicSounds.value;
+
+    }
+
+    public void SetInvertCamYCheckSign(Toggle value)
+    {
+        InvertCamYCheckSign = value;
+        _isCamYInverted = value.isOn;
+        AxisState temp = MainCam.m_YAxis;
+        temp.m_InvertInput = value.isOn;
+        MainCam.m_YAxis = temp;
+    }
+
+}
