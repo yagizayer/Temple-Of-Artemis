a85251d4721532a372ad7934ba99ed71a89a0d02
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-06-16 11:49:36 +0300

    Created Sound Manager script

---
+++ Assets/Scripts/Global Scope/SoundManagement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagement : MonoBehaviour
+{
+    public void RaiseOrLowerEffectSounds(){
+        
+    }
+    public void RaiseOrLowerMusicSounds(){
+
+    }
+}
